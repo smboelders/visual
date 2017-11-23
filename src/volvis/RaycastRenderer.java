6c4459487fa2e6c96acc35a363edc877f47a1be5
Unknown <s.m.boelders@student.tue.nl>
2017-11-23 16:51:59 +0100

    sd

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -233,7 +233,7 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
                     if (val > maxVal){
                         maxVal = val;
                     }
-                //this :)
+                //this sucks
                 }
 
                 // Map the intensity to a grey value by linear scaling
